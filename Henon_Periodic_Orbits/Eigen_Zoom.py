318576bf5f2c6af64dfbed467aee1458f116a611
andysam151 <andysam151@gmail.com>
2019-02-06 09:11:08 -0600

    2/6/19 Eigen Update

--- Henon_Periodic_Orbits/Eigen_Zoom.py
+++ Henon_Periodic_Orbits/Eigen_Zoom.py
@@ -13,10 +13,37 @@ def random_array(min, max, length):
         random_point_x = random.uniform(min[0], max[0])
         random_point_y = random.uniform(min[1], max[1])
         list_of_points.append([random_point_x, random_point_y])
-
     return list_of_points
+
 
 def henon_attractor(a, b, xx, yy):
     give_x = (yy + (a-(xx*xx)))
     give_y = (b*xx)
     return give_x, give_y
+
+
+def chose_first(elem):
+    done = []
+    for n in range(len(elem)):
+        done.append(elem[n][0])
+    return done
+
+
+def chose_second(elem):
+    done = []
+    for n in range(len(elem)):
+        done.append(elem[n][1])
+    return done
+
+
+points_to_plot = random_array([x_range[0], y_range[0]], [x_range[1], y_range[1]], number_of_points)
+plt.plot(chose_first(points_to_plot), chose_second(points_to_plot), 'k,')
+plt.show()
+new_points_to_plot = []
+
+for h in range(len(points_to_plot)):
+    new_x, new_y = henon_attractor(1.4,0.3,points_to_plot[h][0],points_to_plot[h][1])
+    new_points_to_plot.append([new_x, new_y])
+
+plt.plot(chose_first(new_points_to_plot),chose_second(new_points_to_plot),'r,')
+plt.show()
