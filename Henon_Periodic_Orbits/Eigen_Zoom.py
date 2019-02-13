b36cfa71a2e3e0558d9c5c90e293f0a94f995afc
andysam151 <andysam151@gmail.com>
2019-02-13 08:07:35 -0600

    2/13/19 Update

--- Henon_Periodic_Orbits/Eigen_Zoom.py
+++ Henon_Periodic_Orbits/Eigen_Zoom.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import random
+import math
 
 eigenvalue = tuple(float(k) for k in input("eigenvalues x,y: ").split(', '))
 x_range = tuple(float(i) for i in input("x range: ").split(', '))
@@ -46,4 +47,15 @@ for h in range(len(points_to_plot)):
     new_points_to_plot.append([new_x, new_y])
 
 plt.plot(chose_first(new_points_to_plot),chose_second(new_points_to_plot),'r,')
+plt.show()
+final_points_to_plot = []
+
+for n in range(len(new_points_to_plot)):
+    a_value_1 = eigenvalue[0] * math.sqrt(1 / (math.pow(eigenvalue[0], 2) + (4*math.pow(new_points_to_plot[n][0], 2)) + (4*eigenvalue[0]*new_points_to_plot[n][0]) + 1))
+    a_value_2 = eigenvalue[1] * math.sqrt(1 / (math.pow(eigenvalue[1], 2) + (4*math.pow(new_points_to_plot[n][0], 2)) + (4*eigenvalue[1]*new_points_to_plot[n][0]) + 1))
+    new_xx = -1.5838962679253066 + ((new_points_to_plot[n][0]-1.5838962679253066)/a_value_1)/a_value_2
+    new_yy = -0.4751688803775919+((new_points_to_plot[n][1]-0.4751688803775919)/(a_value_1*(eigenvalue[0]+(2*new_points_to_plot[n][0]))))/(a_value_2*(eigenvalue[1]+(2*new_points_to_plot[n][0])))
+    final_points_to_plot.append([new_xx, new_yy])
+
+plt.plot(chose_first(final_points_to_plot),chose_second(final_points_to_plot),'r,')
 plt.show()
